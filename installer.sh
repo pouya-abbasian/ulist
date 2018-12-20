a0dc6d9331a7a8ae532b6a927acee413be4d6aed
Pouya Abbasian <matprogramm3r@gmail.com>
2018-12-20 22:33:48 +0330

    Added installer file, fixed many bugs, create new bugs... v1.2

---
+++ installer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+echo -e 'The YouTube-List V1..\nStart Installing...'
+echo 'Copying File...'
+sudo cp ulist /usr/bin/ulist
+echo 'chmod 775 /usr/bin/ulist is running...'
+sudo chmod 755 /usr/bin/ulist
+echo 'ulist, is installed on your system, you can use that with "ulist" command! GL ;)'
